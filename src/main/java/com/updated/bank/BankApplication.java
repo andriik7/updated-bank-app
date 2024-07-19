c2e6b768c7e623cff548c971efe7e36efe5db53b
Andrii Kuchera <akuchera7@gmail.com>
2024-07-19 15:33:56 +0300

    V1 -- added very basic security properties

--- src/main/java/com/updated/bank/BankApplication.java
+++ src/main/java/com/updated/bank/BankApplication.java
@@ -2,6 +2,7 @@ package com.updated.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 
 @SpringBootApplication
 public class BankApplication {
