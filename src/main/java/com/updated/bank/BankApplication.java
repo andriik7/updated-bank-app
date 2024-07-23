58bd78bbd7b094dacc2d0e66571a1e7bc68393ba
Andrii Kuchera <akuchera7@gmail.com>
2024-07-23 13:38:21 +0300

    V5 -- changed security PasswordEncoder to BCryptPasswordEncoder

--- src/main/java/com/updated/bank/BankApplication.java
+++ src/main/java/com/updated/bank/BankApplication.java
@@ -2,8 +2,10 @@ package com.updated.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.updated.bank.repository")
 public class BankApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/updated/bank/security/SecurityConfig.java
+++ src/main/java/com/updated/bank/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
@@ -32,6 +32,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder encoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
